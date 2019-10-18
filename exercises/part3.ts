273bdf6c7a0ad4676f12cec581c4059cabf3d69c
Katie Layne <klayne1129@gmail.com>
2019-10-17 20:26:11 -0500

    finished exercises

--- exercises/part3.ts
+++ exercises/part3.ts
@@ -7,10 +7,18 @@ let kilometersToMars: number = 225000000;
 let kilometersToTheMoon: number = 384400;
 let milesPerKilometer: number = 0.621;
 
-// Code the "getDaysToLocation" function here:
 
+// Code the "getDaysToLocation" function here:
+function getDaysToLocation(kilometersAway: number): number {
+    let milesAway: number = kilometersAway * milesPerKilometer;
+    let hours: number = milesAway/ speedMph;
+    let days: number = hours / 24;
+    return days;
+}
 
 
 
 
 // Call the function and print the outputs for the Mars trip and the moon trip:
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToMars)} days to get to Mars.`);
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToTheMoon)} days to get to the moon.`);
--- exercises/part4.ts
+++ exercises/part4.ts
@@ -15,11 +15,27 @@ let kilometersToTheMoon: number = 384400;
 // }
 
 // Define your Spacecraft class here:
-
+class Spacecraft {
+    name: string;
+    speedMph: number;
+    milesPerKilometer: number 
+    constructor(name: string, speedMph: number) {
+    this.name = name;
+    this.speedMph = speedMph;
+    this.milesPerKilometer = 0.621;
+    }
+    getDaysToLocation(kilometersAway: number): number {
+        let milesAway: number = kilometersAway * this.milesPerKilometer;
+        let hours: number = milesAway/ this.speedMph;
+        let days: number = hours / 24;
+        return days;
+    }
+}
 
 
 // Create an instance of the class here:
-
+let spaceShuttle = new Spacecraft('Determination', 17500);
 
 // Print two outputs - one for the trip to Mars and one for the trip to the moon.
-
+console.log(`${spaceShuttle.name} would take ${spaceShuttle.getDaysToLocation(kilometersToMars)} days to get to Mars.`);
+console.log(`${spaceShuttle.name} would take ${spaceShuttle.getDaysToLocation(kilometersToTheMoon)} days to get to the moon.`);
